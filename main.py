ac78e97d487884095a071cd0f77990a8348ec857
01000011-00110111 <alfredburford@icloud.com>
2023-11-17 21:04:57 -0500

    you can now send as many commands in a subprocess

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-from flask import Flask, render_template, request
+import fcntl
+import os
+import select
 import subprocess
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+p = subprocess.Popen(['/bin/bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/execute', methods=['POST'])
-def execute_command():
-    command = request.form['command']
+@socketio.on('command')
+def execute_command(command):
     result = execute(command)
-    return render_template('index.html', result=result)
+    socketio.emit('result', result)
 
 def execute(command):
     try:
-        result = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.STDOUT)
+        p.stdin.write(command + '\n')
+        p.stdin.flush()
+        result = terminal_out()
         return result
     except subprocess.CalledProcessError as e:
         return f"Error: {e.output}"
+    
+def terminal_out():
+    while True:
+        rlist, _, _ = select.select([p.stdout, p.stderr], [], [], 0.1)
+        if not rlist:
+            break
+            
+        for stream in rlist:
+            output = os.read(stream.fileno(), 4096).decode('utf-8')
+        return [line.rstrip() for line in output.split('\n') if line.rstrip()]
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True)
