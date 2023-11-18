969709f62553c04bdbfcede3fed33ca081c12212
01000011-00110111 <alfredburford@icloud.com>
2023-11-18 15:30:31 -0500

    added typing and improved the UI

--- static/script.js
+++ static/script.js
@@ -1,25 +1,45 @@
-const socket = io();
-
 // Wrap the code in a window load event to ensure the DOM is fully loaded
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function () {
+    const socket = io();
     // Create xterm.js terminal and attach it to the "terminal" div
+    const terminal = document.getElementById('terminal');
     const term = new Terminal();
-    term.open(document.getElementById('terminal'));
+    term.open(terminal);
 
     // socketio
     socket.on("result", (result) => {
         renderResult(result);
     });
 
+    term.prompt = () => {
+        term.write('\r\n$ ');
+    };
+
+    var curr_line = '';
+    // Listen for user input
+    term.onKey(e => {
+        const key = e.domEvent.key;
+        const letter = e.domEvent.key[0];
+
+        // console.log(key.charCodeAt(0))
+        if (key === 'Enter') {// Enter key pressed, log curr_line, send to server and reset
+            socket.emit('command', curr_line);
+            curr_line = '';
+        } else if (key === 'Backspace') {// delete when backspace is pressed
+            if (term.x > 2) {
+                curr_line = curr_line.slice(0, -1);
+                term.write('\b \b');
+            }
+        } else {// display the current letters
+            curr_line += letter;
+            term.write(key);
+        }
+    });
+
     function renderResult(value) {
-        term.writeln('Result:');
+        term.writeln('\nResult:');
         for (let text of value) {
             term.writeln(`   ${text}`);
         }
     }
-};
-
-function sendCommand() {
-    let command = document.getElementById("command").value;
-    socket.emit('command', command);
-}
+});
