3f77f59035b2ae7e70dc30b7f550f666baa1e8d6
01000011-00110111 <alfredburford@icloud.com>
2023-11-18 14:43:42 -0500

    added a better ui

--- static/script.js
+++ static/script.js
@@ -1,22 +1,25 @@
 const socket = io();
 
-function sendCommand() {
-    let command = document.getElementById("command")["value"];
+// Wrap the code in a window load event to ensure the DOM is fully loaded
+window.onload = function() {
+    // Create xterm.js terminal and attach it to the "terminal" div
+    const term = new Terminal();
+    term.open(document.getElementById('terminal'));
 
+    // socketio
+    socket.on("result", (result) => {
+        renderResult(result);
+    });
+
+    function renderResult(value) {
+        term.writeln('Result:');
+        for (let text of value) {
+            term.writeln(`   ${text}`);
+        }
+    }
+};
+
+function sendCommand() {
+    let command = document.getElementById("command").value;
     socket.emit('command', command);
 }
-
-function renderResult(value) {
-    let list = document.getElementById("result");
-    let listFixed = '';
-    for (let text of value) {
-        listFixed = `${listFixed}       ${text}<br>`
-    }
-    list["innerHTML"] = list["innerHTML"] + 'result: <br>' + listFixed
-}
-
-// socketio
-
-socket.on("result", (result) => {
-    renderResult(result);
-});
