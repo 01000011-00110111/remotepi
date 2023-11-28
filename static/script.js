558f0fae828f64a5f4eee2d5f534b40824981c02
01000011-00110111 <alfredburford@icloud.com>
2023-11-27 20:36:25 -0500

    moved the js code to the js file

--- static/script.js
+++ static/script.js
@@ -1,46 +1,46 @@
-// Wrap the code in a window load event to ensure the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function () {
-    const socket = io();
-    // Create xterm.js terminal and attach it to the "terminal" div
-    const terminal = document.getElementById('terminal');
-    const term = new Terminal();
-    term.open(terminal);
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+var term = new Terminal();
+var fitAddon = new FitAddon.FitAddon();
 
-    // socketio
-    socket.on("result", (result) => {
-        renderResult(result);
-    });
+term.loadAddon(fitAddon);
+term.open(document.getElementById('terminal'));
+fitAddon.fit();
 
-    term.prompt = () => {
-        term.write('\r\n$ ');
-    };
+term.prompt = (prompt) => {
+    term.write(`\r\n${prompt} `);
+};
 
-    let curr_line = '';
-    // Listen for user input
-    term.onKey(e => {
-        const key = e.domEvent.key;
-        // const letter = e.domEvent.key[0];
+term.writeln('remotepi v0.1.0-POC.3');
+term.prompt();
 
-        // console.log(key.charCodeAt(0))
-        if (key === 'Enter') {// Enter key pressed, log curr_line, send to server and reset
-            socket.emit('command', curr_line);
-            curr_line = '';
-        } else if (key === 'Backspace') {// delete when backspace is pressed
-            console.log('pressed ' + curr_line.length)
-            if (curr_line.length > 0) {
-                curr_line = curr_line.slice(0, -1);
-                term.write('\b \b');
-            }
-        } else {// display the current letters
-            curr_line += key;
-            term.write(key);
+let currentInput = '';
+
+term.onKey(e => {
+    const printable = !e.altKey && !e.altGraphKey && !e.ctrlKey && !e.metaKey;
+
+    if (e.domEvent.keyCode === 13) {
+        // Enter key
+        const inputBytes = new TextEncoder().encode(currentInput);
+        socket.emit('input', inputBytes);
+        currentInput = '';  // Clear the current input for the next command
+    } else if (e.domEvent.keyCode === 8) {
+        // Backspace key
+        if (currentInput.length > 0) {
+            currentInput = currentInput.slice(0, -1);
+            term.write('\b \b');
         }
-    });
-
-    function renderResult(value) {
-        term.writeln('\nResult:');
-        for (let text of value) {
-            term.writeln(`   ${text}`);
-        }
+    } else if (printable) {
+        currentInput += e.key;
+        term.write(e.key);
     }
+});
+
+socket.on('output', function (data) {
+    // Split the output into lines
+    const lines = data.output.split('\n');
+
+    // Write each line to the terminal
+    lines.forEach((line, index) => {
+        term.writeln('\r' + line);
+    });
 });
